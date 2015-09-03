b6bafbfabd8725af4d6d4d39e1eaf4da5c34f256
goolkasi18 <goolkasi18@up.edu>
2015-09-03 16:00:58 -0700

    fixed calculation to test for prime

--- app/src/main/java/edu/up/cs371/primetester/PrimeTester.java
+++ app/src/main/java/edu/up/cs371/primetester/PrimeTester.java
@@ -15,6 +15,21 @@ public class PrimeTester {
      */
     public static boolean isPrime(long n) {
         // for now, return a random result
-        return Math.random() > 0.5;
+        n = Math.abs(n);
+        if(n == 0 || n == 1)
+        {
+            return false;
+        }
+        else if(n == 2)
+        {
+            return true;
+        }
+        for (int i = 2; i <= n / 2; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
+
